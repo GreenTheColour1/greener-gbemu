6f0f30e5b9ae6de761275e58eb1619b98c3cbb71
GreenTheColour1 <contact@camerongreen.ca>
2025-03-24 05:58:47 -0400

    add rest of ADD A,x instructions

--- cpu/instructions.go
+++ cpu/instructions.go
@@ -10,5 +10,11 @@ type Instruction struct {
 
 var Instructions []*Instruction = []*Instruction{
 	// Arithmetic Instructions
-	&Instruction{0x80, "ADD A,rB", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.b) }},
+	{0x80, "ADD A,rB", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.b) }},
+	{0x81, "ADD A,rC", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.c) }},
+	{0x82, "ADD A,rD", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.d) }},
+	{0x83, "ADD A,rE", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.e) }},
+	{0x84, "ADD A,rH", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.h) }},
+	{0x85, "ADD A,rL", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.l) }},
+	{0x87, "ADD A,rA", 1, 1, func(cpu *CPU) { cpu.AddA_r(cpu.registers.a) }},
 }
